2aac31f54fcc9043e7a610f7886654be057373c6
fweamy <baohanmi@gmail.com>
2025-01-14 15:43:44 -0500

    Setup Flask directories

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/works")
+def works():
+    return render_template("works.html")
