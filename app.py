056cbbbb02576824627e55f1427a0acd9e2df20f
fweamy <baohanmi@gmail.com>
2025-01-26 16:06:20 -0500

    Add images in lists of dictionaries

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 featured = [
     {
        "title": "Game Projects",
-       "image": "static/images/itch.jpg",
+       "image": "itch.jpg",
         "desc": "RPG and puzzle game projects I have worked on, with a blend of horror and lighthearted narrative!",
         "link": "https://fweamy.itch.io/"
     },
     {
        "title": "Art & Design",
-       "image": "static/images/behance.png",
+       "image": "behance.png",
         "desc": "My graphic design and visual identity work for events and organizations.",
         "link": "https://www.behance.net/fweamy"
     },  
@@ -22,35 +22,35 @@ featured = [
 works = [
     {
        "title": "The Night Embrace",
-       "image": "",
+       "image": "tne.gif",
        "tools": "Unity, C#, Adobe Photoshop",
-        "desc": "A topdown game made with Unity tilemap system that includes an inventory system, game items represetend as objects of classes, and pixel snapping for grahics display.",
+        "desc": "A topdown game made with Unity tilemap system that includes an inventory system, game items represented as objects of classes, and pixel snapping for grahics display.",
         "link": "/"
     },
     {
-       "title": "Pomotivity Tracker",
-       "image": "",
+       "title": "Pomotivity App",
+       "image": "pomotivity.png",
        "tools": "Python, SQL",
         "desc": "A CLI productivity app where users can create, read, update, delete tasks in a to-do list stored in a SQLite3 database and set a task to work on with a pomodoro timer.",
         "link": "/"
     },
     {
        "title": "Personal Website",
-       "image": "",
+       "image": "mysite.png",
        "tools": "Python, HTML, CSS, Javascript, Jinja, Flask framework, Bootstrap framework",
         "desc": "The website you are viewing right now!",
         "link": "/"
     },
     {
        "title": "Sorting Algorithms",
-       "image": "",
+       "image": "sorting.png",
        "tools": "C",
         "desc": "A collection of attempts to implement sorting algorithms in C.",
         "link": "/"
     },
     {
        "title": "Musical Ping Pong",
-       "image": "",
+       "image": "pingpong.png",
        "tools": "Unity, C#",
         "desc": "A ping pong game except every time the ball collides with a surface, it plays a note of the Mii theme.",
         "link": "/"
