5f73ab0653b22612320c5f1ba1d68e357e880f11
fweamy <baohanmi@gmail.com>
2025-01-26 22:45:10 -0500

    Move data to JSON file and storing user's message

--- app.py
+++ app.py
@@ -1,64 +1,26 @@
 from flask import Flask, redirect, render_template, request, session
+import json
+
+# Open JSON file in read mode
+with open('data/main.json', 'r') as file:
+    data = json.load(file)
 
 app = Flask(__name__)
 
-# Creates a list of dictionaries for featured cards
-featured = [
-    {
-       "title": "Game Projects",
-       "image": "itch.jpg",
-        "desc": "RPG and puzzle game projects I have worked on, with a blend of horror and lighthearted narrative!",
-        "link": "https://fweamy.itch.io/"
-    },
-    {
-       "title": "Art & Design",
-       "image": "behance.png",
-        "desc": "My graphic design and visual identity work for events and organizations.",
-        "link": "https://www.behance.net/fweamy"
-    },  
-    ]
-
-# Creates a list of dictionaries for work showcase cards
-projects = [
-    {
-       "title": "The Night Embrace",
-       "image": "tne.gif",
-       "tools": "Unity, C#, Adobe Photoshop",
-        "desc": "A topdown game made with Unity tilemap system that includes an inventory system, game items represented as objects of classes, and pixel snapping for graphics display.",
-        "link": "https://fweamy.itch.io/the-night-embrace"
-    },
-    {
-       "title": "Pomotivity App",
-       "image": "pomotivity.png",
-       "tools": "Python, SQL",
-        "desc": "A CLI productivity app where users can create, read, update, delete tasks in a to-do list stored in a SQLite3 database and set a task to work on with a pomodoro timer.",
-        "link": "https://github.com/fweamy/pomotivity-app"
-    },
-    {
-       "title": "Personal Website",
-       "image": "mysite.png",
-       "tools": "Python, HTML, CSS, Javascript, Jinja, Flask framework, Bootstrap framework",
-        "desc": "The website you are viewing right now!",
-        "link": "https://github.com/fweamy/portfolio-website"
-    },
-    {
-       "title": "Sorting Algorithms",
-       "image": "sorting.png",
-       "tools": "C",
-        "desc": "A collection of attempts to implement sorting algorithms in C.",
-        "link": "https://github.com/fweamy/sorting-algorithms"
-    },
-    {
-       "title": "Musical Ping Pong",
-       "image": "pingpong.png",
-       "tools": "Unity, C#",
-        "desc": "A ping pong game except every time the ball collides with a surface, it plays a note of the Mii theme.",
-        "link": "/"
-    },
-    
-]
-
 @app.route("/")
 def index():
-    return render_template("index.html", featured=featured, projects=projects)
+    return render_template("index.html", featured=data["featured"], projects=data["projects"])
 
+@app.route("/message", methods=['GET', 'POST'])
+def get_message():
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+        
+        with open('data/msg.json', 'r+') as file:
+            user_msg = json.load(file)
+            user_msg['messages'].append(request.form)
+            file.seek(0)
+            json.dump(user_msg, file, indent = 4)
+        
+        return redirect("/contact")
